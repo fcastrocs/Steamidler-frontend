90a8d27e90c7ca66574e49c7d67b1f1b047ded12
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-08-13 01:20:55 -0400

    set proxy correctly

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NODE_ENV === "production" ? HOST[0] : HOST[1]}/:path*`,
+        destination: `${process.env.NODE_ENV === "production" ? HOST[1] : HOST[0]}/:path*`,
       },
     ];
   },
