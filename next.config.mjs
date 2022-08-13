24d24933928c311947a7bb7232f4aaff6f9c659b
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-08-12 23:06:16 -0400

    next.js ready

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const HOST = ["http://localhost:8000", "https://api.steamidler.com"];
+
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NODE_ENV === "production" ? HOST[0] : HOST[1]}/:path*`,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
